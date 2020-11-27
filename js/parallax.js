f2fee924c24929883e9e43b9006417cd4d040f7f
MsRobynM <robyn628@tds.net>
2020-11-27 05:03:49 -0500

    Initial commit of baseline experiment

---
+++ js/parallax.js
@@ -0,0 +1,17 @@
+$(window).scroll(function() {
+  parallax();
+})
+
+function parallax() {
+
+  var wScroll = $(window).scrollTop()
+
+
+  $('.parallax--bg').css('background-position', 'center ' + (wScroll*0.75)+'px');
+
+  $('.parallax--box').css('top', -5 + (wScroll*.005)+'em');
+
+  $('.parallax--box2').css('bottom', -4 - (wScroll*.015)+'em');
+
+  $('.parallax--box3').css('top', -5 - (wScroll*.015)+'em')
+}
